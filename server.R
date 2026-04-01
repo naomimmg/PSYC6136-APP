41ea2b5fdbb69605b87e5d9bcb90ecc1ac4a24c6
naomimmg <72946482+naomimmg@users.noreply.github.com>
2026-04-01 02:17:06 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -1,32 +1,146 @@
 server <- function(input, output, session) {
-  
-  select_dat <- reactive({
-    data_sets[[input$select_sample_dat]]
-  })
-  
-  output$table_preview <- renderUI({
-    vcdExtra::color_table(select_dat())
-  })
-  
-  preview_levels <- reactive(
-    select_dat() |>  detect_levels(is_table = TRUE)
-  )
-  
-  output$level_preview <- renderPrint({
-    cat(paste(
-      lapply(names(preview_levels()), function(nm) {
-        paste0(
-          nm, ": ", paste(preview_levels()[[nm]], collapse = ", ")
+    
+    disableTabs <- function() {
+        runjs("$('.nav-tabs li').addClass('disabled');")
+        runjs(sprintf("$('.nav-tabs li a[data-value=\"%s\"]').parent().removeClass('disabled').addClass('active');", input$tabs))
+    }
+    observeEvent(input$tabs, { disableTabs() })
+    observeEvent(input$next1, { updateTabsetPanel(session, "tabs", selected = "tab2") })
+    observeEvent(input$back2, { updateTabsetPanel(session, "tabs", selected = "tab1") })
+    observeEvent(input$next2, { updateTabsetPanel(session, "tabs", selected = "tab3") })
+    observeEvent(input$back3, { updateTabsetPanel(session, "tabs", selected = "tab2") })
+    observeEvent(input$next3, { updateTabsetPanel(session, "tabs", selected = "tab4") })
+    observeEvent(input$back4, { updateTabsetPanel(session, "tabs", selected = "tab3") })
+    
+    uploaded_data <- reactive({
+        req(input$upload)
+        ext <- tools::file_ext(input$upload$name)
+        switch(ext,
+               csv = vroom::vroom(input$upload$datapath, delim = ","),
+               tsv = vroom::vroom(input$upload$datapath, delim = "\t"),
+               validate("Invalid file; Please upload a .csv or .tsv file"))
+    })
+    
+    output$var_select_inputs <- renderUI({
+        req(uploaded_data())
+        choices <- names(uploaded_data())
+        tagList(
+            selectInput("select_freq", "Frequency Variable:", choices = choices),
+            selectInput("select_variables", "Categorical Variables:", choices = choices, multiple = TRUE)
         )
-      }),
-      collapse = "\n\n"
-    ))
-  })
-  
-  output$mosaic_plot <- renderPlot({
-    vcd::mosaic(select_dat(), 
-                shading = TRUE,
-                split_vertical = "V" %in% input$split,
-                main = paste("Mosaic Plot of", input$select_sample_dat))
-  })
+    })
+    
+    select_dat <- reactive({
+        if (input$data_option == "Use sample data") {
+            req(input$select_sample_dat)
+            data_sets[[input$select_sample_dat]]
+        } else {
+            req(uploaded_data(), input$select_freq, input$select_variables)
+            form <- as.formula(paste(input$select_freq, "~", paste(input$select_variables, collapse = " + ")))
+            xtabs(form, data = uploaded_data())
+        }
+    })
+    
+    output$table_preview <- renderUI({
+        req(select_dat())
+        color_table(select_dat(), legend = "")
+    })
+    
+    preview_levels <- reactive({
+        req(select_dat())
+        detect_levels(select_dat(), is_table = TRUE)
+    })
+    
+    output$level_preview <- renderPrint({
+        req(preview_levels())
+        cat(paste(lapply(names(preview_levels()), function(nm) {
+            paste0(nm, ": ", paste(preview_levels()[[nm]], collapse = ", "))
+        }), collapse = "\n\n"))
+    })
+    
+    observeEvent(select_dat(), {
+        updateSelectInput(session, "select_factors", choices = names(preview_levels()))
+    })
+    
+    mod_dat <- reactive({
+        if (is.null(input$select_factors) || length(input$select_factors) == 0) {
+            select_dat()
+        } else {
+            xtabs(convert_xtabs_formula(input$select_factors), data = select_dat())
+        }
+    })
+    
+    dat_factors <- reactive({
+        names(detect_levels(mod_dat()))
+    })
+    
+    reasonable_formulas <- reactive({
+        show_all_formulas(dat_factors())
+    })
+    
+    output$select_formula_ui <- renderUI({
+        req(input$customize_formula_options == "Select among defaults")
+        selectInput("selected_formula", "Choose a default formula:", choices = reasonable_formulas())
+    })
+    
+    mod_dat_form <- reactive({
+        if (input$customize_formula_options == "Write custom formula") {
+            req(input$custom_formula)
+            f <- try(as.formula(input$custom_formula), silent = TRUE)
+            validate(need(!inherits(f, "try-error"), "Please input a valid formula (e.g., Freq ~ A + B)."))
+            f
+        } else if (input$customize_formula_options == "Select among defaults") {
+            req(input$selected_formula)
+            as.formula(input$selected_formula)
+        } else {
+            convert_loglm_formula(dat_factors())
+        }
+    })
+    
+    observeEvent(input$customize_formula_options, {
+        if (input$customize_formula_options != "No customization") {
+            updateSelectInput(session, "residual_type", choices = c("pearson", "deviance", "rstandard"))
+        } else {
+            updateSelectInput(session, "residual_type", choices = c("pearson", "deviance", "ft"))
+        }
+    })
+    
+    selected_shading <- reactive({
+        switch(input$shading_type,
+               "Friendly" = vcd::shading_Friendly(),
+               "Friendly2" = vcd::shading_Friendly2(),
+               "sieve" = vcd::shading_sieve(),
+               "custom" = vcd::shading_binary(col = c(input$color_1, input$color_2)))
+    })
+    
+    output$mosaic_plot <- renderPlot({
+        req(mod_dat())
+        customized <- input$customize_formula_options != "No customization"
+        args <- list(gp = selected_shading(), split_vertical = "V" %in% input$split)
+        
+        mod.glm <- NULL
+        if (customized) {
+            df <- as.data.frame(mod_dat())
+            mod.glm <- glm(mod_dat_form(), data = df, family = poisson())
+            args$x <- mod.glm
+            args$data <- df
+        } else {
+            args$x <- mod_dat()
+        }
+        
+        if (input$show_residuals) {
+            args$labeling <- labeling_residuals()
+            args$residuals_type <- input$residual_type
+        }
+        
+        main_txt <- NULL
+        if (input$show_formula) main_txt <- paste(deparse(mod_dat_form()), collapse = "")
+        if (input$show_g_square && customized) {
+            g2_val <- paste0("G² = ", round(deviance(mod.glm), 2))
+            main_txt <- if (is.null(main_txt)) g2_val else paste(main_txt, g2_val, sep = ", ")
+        }
+        if (!is.null(main_txt)) args$main <- main_txt
+        
+        suppressWarnings(do.call(vcd::mosaic, args))
+    })
 }
